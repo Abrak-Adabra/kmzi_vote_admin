e7ac12bd3c00d9cf6ec3568d8c17aa0cbe8d395c
Abrak-Adabra <abrak.adabra@internet.ru>
2025-04-21 07:30:08 +0300

    1.01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import MainPage from './screens/main'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './global.css'
+import MainPage from '@/screens/main'
 
 export default function App() {
     return <MainPage />

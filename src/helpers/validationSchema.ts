067a41a6390239b99272d7e9557b68330a0ac043
Abrak-Adabra <abrak.adabra@internet.ru>
2025-04-27 01:52:15 +0300

    1.09

--- src/helpers/validationSchema.ts
+++ src/helpers/validationSchema.ts
@@ -1,7 +1,10 @@
 import * as Yup from 'yup'
 
 export const validationSchema = Yup.object().shape({
-    question: Yup.string().nonNullable().required('Вопрос должен быть не пустым'),
+    question: Yup.string()
+        .nonNullable()
+        .required('Вопрос должен быть не пустым')
+        .test({ test: (value) => !value?.includes('::'), message: 'Нельзя использовать технические символы' }),
     count: Yup.number().integer('Число должно быть целым').min(4, 'Число должно быть > 3'),
     answers: Yup.array().of(Yup.string().nonNullable().required('Вариант ответа не должен быть пустым')),
 })
